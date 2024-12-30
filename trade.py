b26fd2f7ffa75c19c14c6c80348a5e32a2adf5be
vrelme <vander@elme.eti.br>
2024-12-30 05:37:50 -0300

    20241230 Atualizacao

--- trade.py
+++ trade.py
@@ -1,11 +1,11 @@
 import math
 import numpy as np
-import pandas as pd
+import yfinance as yf
 import matplotlib.pyplot as plt
+import pandas as pd
 import random
 import tensorflow as tf
-import pandas_datareader as pdr
-from tqdm import tqdm_notebook, tqdm
+from tqdm import tqdm
 from collections import deque
 
 class AI_Trader():
@@ -21,14 +21,14 @@ class AI_Trader():
         self.epsilon_decay = 0.995
 
         self.model = self.model_builder()
-
+    
     def model_builder(self):
         model = tf.keras.models.Sequential()
         model.add(tf.keras.layers.Dense(units=32, activation='relu', input_dim=self.state_size))
         model.add(tf.keras.layers.Dense(units=64, activation='relu'))
         model.add(tf.keras.layers.Dense(units=128, activation='relu'))
         model.add(tf.keras.layers.Dense(units=self.action_space, activation='linear'))
-        model.compile(loss='mse', optimizer=tf.keras.optimizers.Adam(lr=0.001))
+        model.compile(loss='mse', optimizer=tf.keras.optimizers.Adam(learning_rate=0.001))
 
         return model
     
@@ -59,8 +59,84 @@ def sigmoid(x):
     return 1 / (1 + math.exp(-x))
 
 def stocks_price_format(n):
-    if n < 0:
-        return "- R$ {0:2f}".format(abs(n))
+    if isinstance(n, pd.Series):
+        value = n.iloc[0]
     else:
-        return "R$ {0:2f}".format(abs(n))
+        value = n
+    return ("- R$" if value < 0 else "R$") + "{0:.2f}".format(abs(value))
+
+        
+def dataset_loader(stock_name):   
+    dataset = yf.download(stock_name)
+    close = dataset['Close']
+    return close
     
+def state_creator(data, timestep, window_size):
+    starting_id = timestep - window_size + 1
+        
+    if starting_id >= 0:
+        windowed_data = data[starting_id:timestep+1].values
+    else:
+        windowed_data = - starting_id * [data.iloc[0]] + list(data.iloc[0:timestep+1].values)
+        
+    state = []
+    for i in range(window_size - 1):
+        state.append(sigmoid(windowed_data[i+1] - windowed_data[i]))
+    return np.array([state]), windowed_data
+    
+
+# Carregar dados
+stock_name = "AAPL"
+data = dataset_loader(stock_name)
+
+# Criar estado inicial
+s = state_creator(data, 0, 5)
+window_size = 10
+
+# Exemplo de uso trader
+episodes = 10
+batch_size = 32
+data_samples = len(data)
+
+trader = AI_Trader(window_size)
+
+print(trader.model.summary())
+
+for episode in range(1, episodes + 1):
+    print(f'Episode: {episode} de {episodes}')
+    state, _ = state_creator(data, 0, window_size + 1)
+    total_profit = 0
+    trader.inventory = []
+    for t in tqdm(range(data_samples)):
+        action = trader.trade(state)
+        next_state, _ = state_creator(data, t+1, window_size + 1)
+        reward = 0
+        
+        if action == 1:
+            trader.inventory.append(data.iloc[t])
+            print(f'AI Trader comprou: {stocks_price_format(data.iloc[t])}')
+        elif action == 2 and len(trader.inventory) > 0:
+            buy_price = trader.inventory.pop(0)
+            current_price = data.iloc[t].item()
+            reward = max(current_price - buy_price.item(), 0)
+            total_profit += current_price - buy_price.item()
+            print(f'AI Trader vendeu: {stocks_price_format(current_price)} Lucro de: {stocks_price_format(current_price - buy_price.item())}')
+        
+        if t == data_samples - 1:
+            done = True
+        else:
+            done = False
+        
+        trader.memory.append((state, action, reward, next_state, done))
+        state = next_state
+        
+        if done:
+            print("########################")
+            print(f'Lucro total estimado: {(total_profit)}')
+            print("########################")
+        
+        if len(trader.memory) > batch_size:
+            trader.batch_train(batch_size)
+    
+    if episode % 10 == 0:
+        trader.model.save("ai_trader_{}.h5".format(episode))
